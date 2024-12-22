8faff7fcea6e7fe8dec0139b3cbcf1be6606c6f6
Aditya Kulkarni <beastwolftheleader@gmail.com>
2024-12-22 13:23:37 +0530

    added database string need to give mongouri

--- database/database.go
+++ database/database.go
@@ -4,43 +4,62 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
 	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var DB *mongo.Client
 
-func ConnectDB() {
-	clientOptions := options.Client().ApplyURI("mongodb://")
+func ConnectDB() error {
+	mongoURI := os.Getenv("MONGO_URI")
+	if mongoURI == "" {
+		return fmt.Errorf("MONGO_URI environment variable is not set")
+	}
+
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+
+	clientOptions := options.Client().ApplyURI(mongoURI).SetServerAPIOptions(serverAPI)
+
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
 	client, err := mongo.Connect(ctx, clientOptions)
 	if err != nil {
-		log.Fatalf("Failed to connect to MongoDB: %v", err)
+		return fmt.Errorf("failed to connect to MongoDB: %v", err)
 	}
 
-	err = client.Ping(ctx, nil)
+	err = client.Database("admin").RunCommand(ctx, bson.D{{"ping", 1}}).Err()
 	if err != nil {
-		log.Fatalf("Failed to ping MongoDB: %v", err)
+		return fmt.Errorf("failed to ping MongoDB: %v", err)
 	}
 
-	fmt.Println("Connected to MongoDB!")
 	DB = client
-}
+	fmt.Println("Connected to MongoDB!")
 
+	return nil
+}
 
 func GetCollection(databaseName, collectionName string) *mongo.Collection {
 	return DB.Database(databaseName).Collection(collectionName)
 }
 
-func DisconnectDB() {
+func DisconnectDB() error {
+	if DB == nil {
+		return fmt.Errorf("no active database connection")
+	}
+
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	if err := DB.Disconnect(ctx); err != nil {
-		log.Fatalf("Error disconnecting from MongoDB: %v", err)
+
+	err := DB.Disconnect(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to disconnect from MongoDB: %v", err)
 	}
+
 	fmt.Println("Disconnected from MongoDB")
+	return nil
 }
